4154633756a937aae0298018ff101150a5218b98
Syed Muhammad Amer Ali Zaidi <amir.ali2036a@gmail.com>
2023-12-24 18:13:22 +0500

    fix import error

--- front-end/src/routes/LoginRoute.jsx
+++ front-end/src/routes/LoginRoute.jsx
@@ -2,7 +2,7 @@
 import { lazy } from "react";
 import LoginLayout from "../layout/LoginLayout";
 //pages
-const Login = lazy(() => import("../pages/auth"));
+const Login = lazy(() => import("../pages/login"));
 
 const LoginRoute={
   path: "/",
